ad226f1059993507c89fd38842bde2f8a7a24c1b
Opalecky <adam@opalecky.cz>
2022-07-02 09:35:24 +0200

    init2

---
+++ shared.env.js
@@ -0,0 +1,8 @@
+// Create local.env.js file and replace empty strings with your actual values
+
+export default {
+	PERMISSION_NUMBER: "",//[YOUR_PERMISSIONS_HERE]
+	APP_ID: "", // YOUR_APP_ID
+	PUBLIC_KEY: "", // YOUR_PUBLIC_KEY
+	TOKEN: ""// YOUR_BOT_TOKEN
+};
